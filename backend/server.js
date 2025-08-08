2eb61a01f7fcb131ea4b1cefd4aa055da1485cd0
matthewsays23 <jason.jones06@outlook.com>
2025-08-08 11:40:14 -0400

    Yes

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,6 @@ const corsOptions = {
   credentials: false
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(express.json());
 
 app.use("/auth", authRoutes);    // ✅ all relative paths
